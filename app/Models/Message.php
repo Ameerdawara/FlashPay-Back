f0e0c55d74e678fbbe9c6b5a45739dc8a2ad1cee
Ghaieth Kahel <ghaiethka@gmail.com>
2026-03-17 22:01:32 +0400

    chat

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,18 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    protected $fillable = ['conversation_id', 'sender_id', 'message_text', 'is_read'];
-    public function conversation()
+    use HasFactory;
+
+    protected $fillable = [
+        'transfer_id',
+        'sender_id',
+        'receiver_id',
+        'message',
+        'is_read',
+    ];
+
+    // علاقة الرسالة بالحوالة
+    public function transfer()
     {
-        return $this->belongsTo(Conversation::class);
+        return $this->belongsTo(Transfer::class);
     }
 
+    // علاقة الرسالة بالمرسل
     public function sender()
     {
         return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    // علاقة الرسالة بالمستقبل
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
     }
 }
