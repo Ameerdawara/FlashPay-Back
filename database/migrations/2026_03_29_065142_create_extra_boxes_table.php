eeaaca86ed1312404d67dee4a2f0ca0db9f04ba3
ameerdwdev <dawaraameer14@gmail.com>
2026-03-29 10:56:02 +0400

    extra box

---
+++ database/migrations/2026_03_29_065142_create_extra_boxes_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('extra_boxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // اسم الصندوق الإضافي
+            $table->decimal('amount', 15, 2)->default(0.00); // الكمية أو الرصيد
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('extra_boxes');
+    }
+};
