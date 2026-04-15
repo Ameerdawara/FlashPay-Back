b0c2a949c80ed99dac23b8565f0d6ca46d881186
Ameerdawara <154762129+Ameerdawara@users.noreply.github.com>
2026-04-15 18:54:21 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/debug-db', function() {
+    $tables = DB::select("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'");
+    return response()->json($tables);
+});
