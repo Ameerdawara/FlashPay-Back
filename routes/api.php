cb4cec1e32ec19707b29d4cfa1d3e817b4b69aaf
ameerdwdev <dawaraameer14@gmail.com>
2026-04-16 13:23:58 +0400

    seed

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
+
+Route::get('/run-seeds', function() {
+    try {
+        // تنفيذ الـ Seed الأساسي
+       \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+        return response()->json(['message' => 'تم تنفيذ الـ Seeds بنجاح!']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
 /*
 |--------------------------------------------------------------------------
 | Public Routes (المسارات العامة - بدون توكن)
