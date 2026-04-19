8189c2637119d8ec457d2991b9ef5e5dd46b7cab
Ameerdawara <154762129+Ameerdawara@users.noreply.github.com>
2026-04-19 14:19:18 +0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -234,7 +234,8 @@ Route::middleware('auth:sanctum')->group(function () {
     // ─── 5. Agent (المندوب) فقط ────────────────────────────────────────────
     Route::middleware('role:agent')->group(function () {
 
-    Route::get('/agent/safe',         [MainSafeController::class, 'agentSafe']);
+        Route::get('/agent/safes',        [MainSafeController::class, 'index']);
+       // Route::get('/agent/safe',         [MainSafeController::class, 'agentSafe']);
         Route::get('/agent/safe-details', [TransferController::class, 'agentSafeDetails']);
     //    Route::get('/bank-transfer',                [BankTransferController::class, 'index']);
     //     Route::get('/bank-transfer/{id}',           [BankTransferController::class, 'show']);
