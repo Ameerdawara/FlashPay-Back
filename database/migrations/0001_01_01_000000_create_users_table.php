8321f54d59386f41fc4d4d312c17d214c75af80d
Kaisalafef <142940796+Kaisalafef@users.noreply.github.com>
2026-04-26 19:13:40 +0300

    W111777لل

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
 
             // ✅ أعمدة ضرورية كانت ناقصة
-            $table->string('fcm_token')->nullable();
             $table->decimal('agent_profit_ratio', 5, 2)->default(0)->nullable();
 
             $table->rememberToken();
